1ccff3b24470fcafa0affd2129faa865e44d3f3c
Ashanti Mutinta <AshantiMutinta@gmail.com>
2018-02-28 15:25:32 -0500

    Added Code For Reading in Device

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::net::UdpSocket;
 use std::time::Duration;
 extern crate time;
-extern crate checksum;
-use checksum::crc32;
+extern crate crc;
+use crc::{crc32, Hasher32};
 
 #[derive(Debug)]
 enum Status {
@@ -23,12 +23,26 @@ enum data_transmittion_error{
     could_not_send_data
 }
 
+#[derive(Debug,PartialEq)]
+enum data_recieve_error{
+    protocol_error,
+    could_not_recieve_data,
+    possible_corrupted_data,
+    empty_buffer
+
+}
+#[derive(Debug,PartialEq)]
+enum checksum_error
+{
+    mismatch
+}
 
 
+
+#[derive(Debug)]
 struct Device
 {
-    ip_address : String,
-    mac_address : String,
+    device_id : u8,
     status : Status
 }
 
@@ -93,12 +107,15 @@ fn set_up_devices(udp_socket : &UdpSocket) -> Vec<Device>
 
 }
 
-fn set_up_command_broadcast() -> [u8;9]
+fn set_up_command_broadcast() -> [u8;11]
 {
     let current_time = time::now();
     let command_without_checksum = [165,5,0,0,current_time.tm_hour as u8,current_time.tm_min as u8,current_time.tm_sec as u8];
     let checksum = get_checksum(&command_without_checksum);
-    [165,5,0,0,current_time.tm_hour as u8,current_time.tm_min as u8,current_time.tm_sec as u8,(checksum>>8) as u8,
+    [165,9,0,0,current_time.tm_hour as u8,current_time.tm_min as u8,current_time.tm_sec as u8, 
+    (checksum>>24) as u8,
+    (checksum>>16) as u8,
+    (checksum>>8) as u8,
     (checksum | 0x00ff) as u8]
 
 
@@ -106,8 +123,7 @@ fn set_up_command_broadcast() -> [u8;9]
 
 fn get_checksum(bytes : &[u8]) -> u32
 {
-    let mut checksum = crc32::Crc32::new();
-    checksum.checksum(bytes)
+    crc32::checksum_ieee(bytes)
 }
 
 
@@ -115,10 +131,107 @@ fn get_checksum(bytes : &[u8]) -> u32
 
 fn retrieve_devices(udp_socket : &UdpSocket) -> Vec<Device>
 {
-     unimplemented!("Retrieve Devices From LAN")
+     unimplemented!("could not retrieve devices")
 }
 
+fn get_device_from_bytes(buffer:&[u8]) -> Result<Device,data_recieve_error>
+{
+    match buffer.first()
+    {
+        Some(first) =>
+        {
+            if(*first==165u8)
+            {
+                match validate_checksum(buffer)
+                {
+                    Ok(validated_buffer) =>
+                    {
+                        match create_buffer_from_device(buffer)
+                        {
+                            Some(result) => Ok(result),
+                            None => Err(data_recieve_error::possible_corrupted_data)
+                        }
 
+                    },
+                    Err(_) =>{Err(data_recieve_error::possible_corrupted_data)}
+                }
+            }
+            else
+            {
+                Err(data_recieve_error::protocol_error)
+            }
+        },
+        None =>Err(data_recieve_error::empty_buffer)
+    }
+    
+
+}
+
+fn validate_checksum(buffer: &[u8]) -> Result<(),checksum_error>
+{
+    if(buffer.len()>4)
+    {
+        let buffer_without_checksum = buffer.split_at(buffer.len()-4);
+        let calculated_read_buffer_checksum = get_checksum(buffer_without_checksum.0);
+        let checksum = ((buffer_without_checksum.1[0] as u32) <<24) |
+                    ((buffer_without_checksum.1[1] as u32)  <<16) |
+                    ((buffer_without_checksum.1[2] as u32)  <<8) |
+                    ((buffer_without_checksum.1[3] as u32) ) ;
+        if(checksum==calculated_read_buffer_checksum)
+        {
+            Ok(())
+        }
+        else
+        {
+            Err(checksum_error::mismatch)
+        }
+    }
+    else
+    {
+        Err(checksum_error::mismatch)
+    }
+    
+    
+
+}
+
+fn create_buffer_from_device(buffer:&[u8]) -> Option<Device>
+{
+    if(buffer.len()>2)
+    {
+        Some(Device
+        {
+             device_id : buffer[2],
+             status : Status::on
+        })
+    
+    }
+    else
+    {
+        None
+    }
+    
+}
+
+#[test]
+fn test_validate_checksum()
+{
+    assert_eq!(validate_checksum(&[165,9,15,1,0,0,0,0x95,0x1c,0x82,0xcb]),Ok(()));
+    assert_eq!(validate_checksum(&[165,9,19,1,0,0,0,0x95,0x1C,0x82,0xCB]).expect_err("Matching checksum"),checksum_error::mismatch);
+
+}
+
+#[test]
+fn test_get_device_from_bytes()
+{
+    
+    assert_eq!(get_device_from_bytes(&[]).expect_err("should expect protocol error"),data_recieve_error::empty_buffer);
+    assert_eq!(get_device_from_bytes(&[00,9,15,1,0,0,0,0x95,0x1C,0x82,0xCB]).expect_err("should expect protocol error"),data_recieve_error::protocol_error);
+    assert_eq!(get_device_from_bytes(&[165,9,15,1,0,0,0,0,0,0,0]).expect_err("expected corrupted data"),data_recieve_error::possible_corrupted_data);
+    assert_eq!(get_device_from_bytes(&[165,9,15,1,0,0,0,0x95,0x1C,0x82,0xCB]).expect("expected device").device_id,15);
+    
+
+}
 
 
 
